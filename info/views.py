a9d2f6d9e0de76404c925aa16d8b7d4ac531c7e6
yomikoyomi <yomi.koyomi@gmail.com>
2016-04-15 00:56:34 +0900

    sessionに詰める

--- info/views.py
+++ info/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+
+def index(request):
+    if 'login_info' not in request.session:
+        return HttpResponse('セッションないよ')
+    return HttpResponse(request.session['login_info'])
--- login/views.py
+++ login/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 
+
 def index(request):
-    """ TODO ログインセッションが存在した場合、infoへ飛ばす """
+    """ ログインセッションが存在した場合、infoへ飛ばす """
+    if 'login_info' in request.session:
+        return HttpResponseRedirect('/bookManagement/info/')
+
     return render(request, 'login/index.html')
 
 
@@ -15,6 +19,8 @@ def auth(request):
 
     if user is not None:
         login(request, user)
-        return HttpResponse('ログイン成功')
+        request.session.set_expiry(0)
+        request.session['login_info'] = user.username
+        return HttpResponseRedirect('/bookManagement/info/')
     else:
         return HttpResponse('ログイン失敗')
