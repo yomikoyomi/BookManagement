b049bc45e3fd759fe9b0adf9cf0ab0e99881914c
yomikoyomi <yomi.koyomi@gmail.com>
2016-04-17 19:02:04 +0900

    infoがまざっちゃったよ

--- BookManagement/urls.py
+++ BookManagement/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^bookManagement/login/', include('login.urls', namespace='login')),
-    url(r'^bookManagement/info/', include('info.urls', namespace='login')),
+    url(r'^bookManagement/info/', include('info.urls', namespace='info')),
 ]
--- info/views.py
+++ info/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect
 
+from info.models import trn_infomation
 
 def index(request):
     if 'login_info' not in request.session:
         return HttpResponse('セッションないよ')
-    return HttpResponse(request.session['login_info'])
+
+    infomation = trn_infomation.objects.all().order_by('id').reverse()
+    return render(request, 'info/index.html',
+    {'infomation': infomation})
+#    return HttpResponse(request.session['login_info'])
+
+
